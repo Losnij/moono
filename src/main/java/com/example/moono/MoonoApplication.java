aeec3441956bfd8693ab41c26da7f7ff09645caf
Losnij <jinsol5571@gmail.com>
2024-11-15 14:37:32 +0900

    Initial commit

---
+++ src/main/java/com/example/moono/MoonoApplication.java
@@ -0,0 +1,13 @@
+package com.example.moono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moono/MoonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
