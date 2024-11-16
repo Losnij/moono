e784d2cb24e3722d62f2fba7043adffdd9b5c1ec
Losnij <jinsol5571@gmail.com>
2024-11-16 15:51:24 +0900

    empty

--- src/main/java/com/example/moono/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.moono.repository;
-
-import com.example.moono.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
